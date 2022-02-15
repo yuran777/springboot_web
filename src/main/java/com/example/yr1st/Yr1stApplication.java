cfd47c620b761a300fd13d20ec94a3cdf6fae83c
yuran777 <yuran3391@gmail.com>
2022-02-15 16:47:31 +0900

    environment setting

---
+++ src/main/java/com/example/yr1st/Yr1stApplication.java
@@ -0,0 +1,13 @@
+package com.example.yr1st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yr1stApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yr1stApplication.class, args);
+	}
+
+}
